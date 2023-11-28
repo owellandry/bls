645d8346d236a9d69822463dc63cf62da78099d1
jogebro <joelabril2110@gmail.com>
2023-11-28 11:53:46 -0500

    feat: :sparkles: link to projects

--- src/components/Home/btnLinks/btnLinks.jsx
+++ src/components/Home/btnLinks/btnLinks.jsx
@@ -7,6 +7,89 @@ export default function BtnLinks(){
             <div className="titleLink">
                 <h1>Conoce Nuestros <span className="gradient-text">Proyectos</span></h1>
             </div>
+            <div className="links">
+                <div className="link">
+                    <div>
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_676_2548)">
+                            <path d="M39.5833 43.7505V38.0817L38.4125 37.1442C33.4708 33.1922 33.3333 29.1109 33.3333 21.9213H37.5L37.5125 15.6713H32.3979C33.0413 14.485 33.3826 13.1584 33.3916 11.8088C33.3916 9.21716 31.4812 5.95882 29.177 2.28174C28.74 1.58439 28.133 1.0095 27.4129 0.610989C26.6928 0.21248 25.8834 0.00341797 25.0604 0.00341797C24.2374 0.00341797 23.4279 0.21248 22.7078 0.610989C21.9878 1.0095 21.3807 1.58439 20.9437 2.28174C18.6291 5.96091 16.7208 9.21924 16.7208 11.8067C16.7297 13.1437 17.0651 14.4582 17.6979 15.6359L12.5 15.6255V21.8755H16.6666C16.6666 29.0901 16.5229 33.1859 11.5875 37.1359L10.4166 38.0817V43.7505H6.30412V50.0005H43.8041V43.7505H39.5833ZM27.0833 21.9067C27.0833 27.6547 27.1979 32.8317 30.2937 37.5005H19.7062C22.7979 32.8359 22.9166 27.6526 22.9166 21.8984L27.0833 21.9067Z" fill="url(#paint0_linear_676_2548)"/>
+                            </g>
+                            <defs>
+                            <linearGradient id="paint0_linear_676_2548" x1="25.0541" y1="0.00341797" x2="25.0541" y2="50.0005" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FF6B01"/>
+                            <stop offset="1" stop-color="#DF1C6E"/>
+                            </linearGradient>
+                            <clipPath id="clip0_676_2548">
+                            <rect width="50" height="50" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div>
+                        <h1>Juegos</h1>
+                    </div>
+                    <div className="despLink">
+                        <p>Descubre Nuestros Proyectos:</p>
+                        <p>Hecha Un Vistazo A La Creatividad Y Trabajo Detrás De Cada Uno.</p>
+                    </div>
+                </div>
+                <div className="link">
+                    <div>
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_676_2538)">
+                            <path d="M50 18.125L25 3.125L0 18.125V20.4167L25 35.4167L50 20.4167V18.125ZM10.2417 19.2708L25 10.4167L39.7583 19.2729L25 28.125L10.2417 19.2708Z" fill="url(#paint0_linear_676_2538)"/>
+                            <path d="M50 24.5867L25 39.5867L0 24.5867V31.8763L25 46.8763L50 31.8763V24.5867Z" fill="url(#paint1_linear_676_2538)"/>
+                            </g>
+                            <defs>
+                            <linearGradient id="paint0_linear_676_2538" x1="25" y1="3.125" x2="25" y2="35.4167" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FF6B01"/>
+                            <stop offset="1" stop-color="#DF1C6E"/>
+                            </linearGradient>
+                            <linearGradient id="paint1_linear_676_2538" x1="25" y1="24.5867" x2="25" y2="46.8763" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FF6B01"/>
+                            <stop offset="1" stop-color="#DF1C6E"/>
+                            </linearGradient>
+                            <clipPath id="clip0_676_2538">
+                            <rect width="50" height="50" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <div>
+                        <h1>Apps</h1>
+                    </div>
+                    <div className="despLink">
+                        <p>Descubre Nuestros Proyectos:</p>
+                        <p>Hecha Un Vistazo a Nuestras Aplicaciones Y El Proceso Detras De Cada Una.</p>
+                    </div>
+                </div>
+                <div className="link">
+                    <div>
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_676_2544)">
+                            <path d="M50 39.5846V10.418C50 8.76037 49.3415 7.17065 48.1694 5.99855C46.9973 4.82645 45.4076 4.16797 43.75 4.16797H6.25C4.5924 4.16797 3.00268 4.82645 1.83058 5.99855C0.65848 7.17065 0 8.76037 0 10.418L0 39.5846H21.875V43.7513H13.5417V50.0013H36.4583V43.7513H28.125V39.5846H50ZM6.25 10.418H43.75V33.3346H6.25V10.418Z" fill="url(#paint0_linear_676_2544)"/>
+                            </g>
+                            <defs>
+                            <linearGradient id="paint0_linear_676_2544" x1="25" y1="4.16797" x2="25" y2="50.0013" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FF6B01"/>
+                            <stop offset="1" stop-color="#E720BB"/>
+                            </linearGradient>
+                            <clipPath id="clip0_676_2544">
+                            <rect width="50" height="50" fill="white"/>
+                            </clipPath>
+                            </defs>
+                        </svg>
+
+                    </div>
+                    <div>
+                        <h1>Paginas</h1>
+                    </div>
+                    <div className="despLink">
+                        <p>Descubre Nuestros Proyectos:</p>
+                        <p>Hecha Un Vistazo A Nuestras Paginas Web Y Enfocados En UI / UX</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
